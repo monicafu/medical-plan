7bfd5265762337eaa7eb8d99e2c8a5b1fabbfd4b
monicafu <31578677+monicafu@users.noreply.github.com>
2019-06-03 21:07:39 -0700

    delete some useless import

--- src/main/java/com/medical/plan/demo/DemoApplication.java
+++ src/main/java/com/medical/plan/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.medical.plan.demo;
 
-import com.medical.plan.demo.model.Plan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
